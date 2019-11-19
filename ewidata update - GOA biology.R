9e64c4f78a7ade662350bc96cac048d864c596e4
Mike Litzow <mlitzow@alaska.edu>
2019-11-18 17:37:19 -0900

    Updated DFA and era-dependent loadings

--- ewidata update - GOA biology.R
+++ ewidata update - GOA biology.R
@@ -139,6 +139,10 @@ GOA <- rbind(GOA, xtra1, xtra2)
 # and save
 write.csv(GOA, "updated GOA biology data.csv", row.names = F)
 
+# reload
+GOA <- read.csv("updated GOA biology data.csv")
+
+
 # and check one more time
 ggplot(GOA, aes(year, value)) +
   theme_bw() +
@@ -146,6 +150,7 @@ ggplot(GOA, aes(year, value)) +
   geom_point() +
   facet_wrap(~code, scales="free_y")
 
+##########################################
 # now fit models!
 library(Rcpp)
 library(ewidata)
@@ -158,6 +163,10 @@ library(gtools)
 # devtools::install_github("fate-ewi/bayesdfa", force=T)
 
 library(bayesdfa)
+
+# reload data if needed
+
+GOA <- read.csv("updated GOA biology data.csv")
 
 mcmc_iter = 4000
 max_trends = 1 # changing to 1!
@@ -214,4 +223,180 @@ capture.output(summary_table, file = paste0(name, "_summary.txt"))
 
 dev.off()
 
+############################################
+# fit to the first and last 15 years, as for climate data
+######################
+# one-trend models fit separately to first 15 and last 15 years of the TS
+sub_data <- GOA %>%
+  select(year, code, value) %>%
+  filter(year <= 1986) 
 
+# ONE! trend model
+# with original data
+max_trends = 1
+name <- "GOA_biol_1_trend_1972_1986"
+
+# reshape data
+melted = melt(sub_data[, c("code", "year", "value")], id.vars = c("code", "year"))
+Y <- dcast(melted, code ~ year)
+names = Y$code
+Y = as.matrix(Y[,-which(names(Y) == "code")])
+
+# do the trend search
+set.seed(99)
+dfa_summary = find_dfa_trends(
+  y = Y,
+  kmax = min(max_trends, nrow(Y)),
+  iter = mcmc_iter,
+  compare_normal = FALSE,
+  variance = c("unequal", "equal"),
+  chains = mcmc_chains
+)
+saveRDS(dfa_summary, file = paste0(name, ".rds"))
+
+# Make default plots (currently work in progress)
+pdf(paste0(name, "_plots.pdf"))
+rotated = rotate_trends(dfa_summary$best_model)
+# trends
+print(plot_trends(rotated, years = as.numeric(colnames(Y))))
+# loadings
+print(plot_loadings(rotated, names = names))
+
+if(ncol(rotated$Z_rot_mean)==2) {
+  plot(rotated$Z_rot_mean[,1], rotated$Z_rot_mean[,2], col="white",
+       xlab="Loading 1", ylab = "Loading 2")
+  text(rotated$Z_rot_mean[,1], rotated$Z_rot_mean[,2], names, cex=0.3)
+  lines(c(-10,10),c(0,0))
+  lines(c(0,0), c(-10,10))
+}
+# predicted values with data
+print(plot_fitted(dfa_summary$best_model,names=names) + 
+        theme(strip.text.x = element_text(size = 6)))
+
+# table of AIC and std errors
+summary_table<-dfa_summary$summary
+capture.output(summary_table, file = paste0(name, "_summary.txt"))
+
+dev.off() 
+
+
+# last 15
+sub_data <- GOA %>%
+  select(year, code, value) %>%
+  filter(year >= 2003) 
+
+# ONE! trend model
+# with original data
+max_trends = 1
+name <- "GOA_biol_1_trend_original_data_2003_2017"
+
+# reshape data
+melted = melt(sub_data[, c("code", "year", "value")], id.vars = c("code", "year"))
+Y <- dcast(melted, code ~ year)
+names = Y$code
+Y = as.matrix(Y[,-which(names(Y) == "code")])
+
+# do the trend search
+set.seed(99)
+dfa_summary = find_dfa_trends(
+  y = Y,
+  kmax = min(max_trends, nrow(Y)),
+  iter = mcmc_iter,
+  compare_normal = FALSE,
+  variance = c("unequal", "equal"),
+  chains = mcmc_chains
+)
+saveRDS(dfa_summary, file = paste0(name, ".rds"))
+
+# Make default plots (currently work in progress)
+pdf(paste0(name, "_plots.pdf"))
+rotated = rotate_trends(dfa_summary$best_model)
+# trends
+print(plot_trends(rotated, years = as.numeric(colnames(Y))))
+# loadings
+print(plot_loadings(rotated, names = names))
+
+if(ncol(rotated$Z_rot_mean)==2) {
+  plot(rotated$Z_rot_mean[,1], rotated$Z_rot_mean[,2], col="white",
+       xlab="Loading 1", ylab = "Loading 2")
+  text(rotated$Z_rot_mean[,1], rotated$Z_rot_mean[,2], names, cex=0.3)
+  lines(c(-10,10),c(0,0))
+  lines(c(0,0), c(-10,10))
+}
+# predicted values with data
+print(plot_fitted(dfa_summary$best_model,names=names) + 
+        theme(strip.text.x = element_text(size = 6)))
+
+# table of AIC and std errors
+summary_table<-dfa_summary$summary
+capture.output(summary_table, file = paste0(name, "_summary.txt"))
+
+dev.off() 
+
+########################################################
+# make an era-specific plot of loadings!
+
+# set colors
+cb <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+# join posteriors for loadings in the two eras
+# load the first 15 year results
+# load the 1-trend object
+GOA.biol.era1 <- readRDS("GOA_biol_1_trend_1972_1986.rds") # read in GOA climate model
+
+# recover the years and names for plots!
+sub_data <- GOA %>%
+  select(year, code, value) %>%
+  filter(year <= 1986) 
+
+melted = melt(sub_data[, c("code", "year", "value")], id.vars = c("code", "year"))
+Y <- dcast(melted, code ~ year)
+names = Y$code
+Y = as.matrix(Y[,-which(names(Y) == "code")])
+
+rotated = rotate_trends(GOA.biol.era1$best_model) 
+
+# set new names
+new.names <- c("Chignik.coho", "Chignik.pink", "Chiniak.eulachon", "Chiniak.jellyfish", 
+               "Chiniak.P.cod", "Chiniak.shrimp", "Cook.Inlet.coho", "Kodiak.coho", "Kodiak.pink", 
+               "Pavlof.capelin", "Pavlof.eulachon", "Pavlof.jellyfish", "Pavlof.P.cod", "Pavlof.shrimp",
+               "Prince.William.coho", "Southeast.coho", "Southeast.herring", "Southeast.pink", "South.Peninsula.coho", "South.Peninsula.pink")
+
+check <- cbind(as.character(names), new.names)
+check
+
+# back to the bespoke code!
+loadings.1 <- as.data.frame(rotated$Z_rot[,,1])
+names(loadings.1)  <- new.names
+
+# now....era2
+GOA.biol.era2 <- readRDS("GOA_biol_1_trend_original_data_2003_2017.rds") # read in GOA climate model
+rotated = rotate_trends(GOA.biol.era2$best_model) 
+loadings.2 <- as.data.frame(rotated$Z_rot[,,1])
+names(loadings.2)  <- new.names
+
+loadings.1$era <- "1972-1986"
+loadings.2$era <- "2003-2017"
+
+
+# plot
+plot.load <- rbind(loadings.1, loadings.2) %>%
+  gather(key, value, -era)
+
+# rank <- tapply(plot.load$value, plot.load$key, mean)
+# plot.load$rank <- rank[match(plot.load$key, names(rank))]
+# 
+# plot.load$key <- reorder(plot.load$key, plot.load$rank)
+
+# set pallette
+cb <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+ggplot(plot.load, aes(value, fill=era)) + 
+  geom_density(alpha=0.5) +
+  scale_fill_manual(values=cb[c(2,4)]) +
+  theme_bw() +
+  facet_wrap(~key, scales = "free_y") +
+  xlim(-2.5, 2.5) + geom_vline(xintercept = 0, lty=2) + 
+  theme(legend.title = element_blank(), legend.position = 'top')
+
+ggsave("era-dependent biology loadings.png", width=8, height=6, units="in")
